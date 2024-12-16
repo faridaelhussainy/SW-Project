4dfea9ca645f5b3fe6ef6a5b84648ca23e23d9aa
Shahdamr-eng <Shahdamr246@icloud.com>
2024-12-16 08:24:01 +0200

    second merging from shahd to farida

---
+++ patient.js
@@ -0,0 +1,8 @@
+
+document.getElementById('update-btn').addEventListener('click', function() {
+    alert('Patient details updated successfully!');
+});
+
+document.getElementById('add-prescription').addEventListener('click', function() {
+    alert('Add Prescription functionality is not implemented yet.');
+});
